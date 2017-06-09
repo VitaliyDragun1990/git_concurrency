ab569f56fab0bf03414e6db26200befcf3f9b15a
Vitaliy Dragun <vdrag00n90@gmail.com>
2017-06-09 17:15:30 +0300

    Transfer method improvement.

--- Concurrency/src/tkach/tutorial/Operations.java
+++ Concurrency/src/tkach/tutorial/Operations.java
@@ -5,6 +5,9 @@ import java.time.LocalTime;
 import java.util.concurrent.TimeUnit;
 
 public class Operations {
+	
+	final static long waitTimeForLock = 500; // 500 millis
+	
 	public static void main(String[] args) throws InsufficientFundsException, InterruptedException {
 		final Account a = new Account(1000, "Account_1");
 		final Account b = new Account(2000, "Account_2");
@@ -30,36 +33,41 @@ public class Operations {
 		if (a.getBalance() < amount) {
 			throw new InsufficientFundsException("Not enought funds to transfer");
 		}
+		boolean isSuccessTransfer = false;
 		
-		// Transfer with locks
-		printLog(" INFO - Trying to lock ", a);
-		if (a.getLock().tryLock(500, TimeUnit.MILLISECONDS)) {
-			printLog(" INFO - Locked ", a);
-			try {
-				printLog(" INFO - Trying to lock ", b);
-				if (b.getLock().tryLock(500, TimeUnit.MILLISECONDS)) {
-					printLog(" INFO - Locked ", b);
-					try {
-						// Do transfer
-						System.out.println("Process transfer operation...");
-						a.withdraw(amount);
-						b.deposit(amount);
-						System.out.println("The transfer is successful.\n"
-								+ "Account status after transfer operation:\n"
-								+ a + " = " + a.getBalance() + ", "
-								+ b + " = " + b.getBalance() + "\n");
-					} finally {
-						b.getLock().unlock();
+		while (!isSuccessTransfer) {
+			// Wait random amount of time between attempt to obtain a lock
+			Thread.sleep((long) (Math.random()*500));
+			// Transfer with locks
+			printLog(" INFO - Trying to lock ", a);
+			if (a.getLock().tryLock(waitTimeForLock, TimeUnit.MILLISECONDS)) {
+				printLog(" INFO - Locked ", a);
+				try {
+					printLog(" INFO - Trying to lock ", b);
+					if (b.getLock().tryLock(waitTimeForLock, TimeUnit.MILLISECONDS)) {
+						printLog(" INFO - Locked ", b);
+						try {
+							// Do transfer
+							System.out.println("Process transfer operation...");
+							a.withdraw(amount);
+							b.deposit(amount);
+							System.out.println(
+									"The transfer is successful.\n" + "Account status after transfer operation:\n" + a
+											+ " = " + a.getBalance() + ", " + b + " = " + b.getBalance() + "\n");
+							isSuccessTransfer = true;
+						} finally {
+							b.getLock().unlock();
+						}
+					} else {
+						printLog(" ERROR - Cannot get a lock for ", b);
 					}
-				} else {
-					printLog(" ERROR - Cannot get a lock for ", b);
+				} finally {
+					a.getLock().unlock();
+
 				}
-			} finally {
-				a.getLock().unlock();
-				
+			} else {
+				printLog(" ERROR - Cannot get a lock for ", a);
 			}
-		} else {
-			printLog(" ERROR - Cannot get a lock for ", a);
 		}
 		
 	}
