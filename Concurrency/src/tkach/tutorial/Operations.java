c9dce02af94568d30a3b1d8996119cb3f15dcba5
Vitaliy Dragun <vdrag00n90@gmail.com>
2017-06-09 15:54:34 +0300

    Added logging print messages.

--- Concurrency/src/tkach/tutorial/Operations.java
+++ Concurrency/src/tkach/tutorial/Operations.java
@@ -1,5 +1,8 @@
 package tkach.tutorial;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class Operations {
 	public static void main(String[] args) throws InsufficientFundsException, InterruptedException {
 		final Account a = new Account(1000, "Account_1");
@@ -26,11 +29,18 @@ public class Operations {
 		if (a.getBalance() < amount) {
 			throw new InsufficientFundsException("Not enought funds to transfer");
 		}
-		
+		System.out.println(LocalDateTime.now() + " ["+Thread.currentThread().getName()+"]" + " INFO - "
+				+ "Trying to lock " + a);
 		synchronized (a) {
+			System.out.println(LocalDateTime.now() + " ["+Thread.currentThread().getName()+"]" + " INFO - "
+					+ "Locked " + a);
 			// Simulating deadlock
-			Thread.sleep(1000);
+			//Thread.sleep(1000);
+			System.out.println(LocalDateTime.now() + " ["+Thread.currentThread().getName()+"]" + " INFO - "
+					+ "Trying to lock " + b);
 			synchronized (b) {
+				System.out.println(LocalDateTime.now() + " ["+Thread.currentThread().getName()+"]" + " INFO - "
+						+ "Locked " + b);
 				a.withdraw(amount);
 				b.deposit(amount);
 			}
